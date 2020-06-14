2df1baa87663d4683c01fab0be514e026cb71bd6
Taciano Morais Silva <tacianosilva@gmail.com>
2020-06-13 22:03:49 -0300

    Add arquivo Shade.dart. Tutorial Java to Dart.

---
+++ lib/Shade.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+abstract class Shape {
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type.';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t create $type.';
+}
+
+main() {
+  final circle = Circle(3);
+  final square = Square(3);
+  print(circle.area);
+  print(square.area);
+  final circle2 = shapeFactory('circle');
+  final square2 = shapeFactory('square');
+  print(circle2.area);
+  print(square2.area);
+}
